b4fbce05aa70a025a4b4405f479fdd8aef7d41ba
kitutukitanuki <60804200+kitutukitanuki@users.noreply.github.com>
2020-02-11 14:31:29 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -3,52 +3,106 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo kitutuki',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(),
+      home: MyHomePage(title: 'わかりやすい'),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('First Page')),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return SecondPage();
-              }),
-            );
-          },
-          child: Text('Next Page'),
-        ),
-      ),
-    );
-  }
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class SecondPage extends StatelessWidget {
+class _MyHomePageState extends State<MyHomePage> {
+  int _count = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Second Page')),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back'),
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text("$_count"),
+              Text("\n"),
+              FlatButton(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                onPressed: () {
+                  setState(() => _count++);
+                },
+                child: Text(
+                  '押してみて',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text("\n\n"),
+              FlatButton(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                onPressed: () {
+                  setState(() => _count--);
+                },
+                child: Text(
+                  '押してたらだめよ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text("\n\n"),
+              FlatButton(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                onPressed: () {
+                  setState(() => _count = _count - 2);
+                },
+                child: Text(
+                  'さらに押してたらだめよ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text('\n\n'),
+              FlatButton(
+                padding: EdgeInsets.all(20.0),
+                color: Colors.red,
+                onPressed: () {
+                  setState(() => _count = 0);
+                },
+                child: Text(
+                  '最初から',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Image.network(
+                  'https://nzigen.com/flutter-reference/assets/img/samples/logo-jumper.png'),
+              Image.asset('images/logo-jumper.png'),
+            ],
+          ),
+        ));
   }
 }
